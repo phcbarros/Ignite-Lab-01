6cf3486307a3649afbfcb2e3879c3c594332b9ff
phcbarros <paulo.barros@outlook.com.br>
2022-06-23 19:46:31 -0300

    Ignite Lab - Aula 2 - Estrutura visual do projeto

--- event-platform/src/App.tsx
+++ event-platform/src/App.tsx
@@ -1,21 +1,9 @@
-import {gql, useQuery} from '@apollo/client'
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`
+import {Event} from './pages/Event'
 
 function App() {
-  const {data} = useQuery(GET_LESSONS_QUERY)
-  console.log(data)
-
   return (
     <div className="App">
-      <h1 className="text-5xl font-bold">Hello World</h1>
+      <Event />
     </div>
   )
 }
